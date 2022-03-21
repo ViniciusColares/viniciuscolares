79a1dbba14f604ef40b1f013d734ce1e48ec77a1
Vinicius Colares <dev@viniciuscolares.com>
2022-03-21 19:24:49 -0300

    :wrench: Commitlint and gitmoji

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional', 'gitmoji'] }
+module.exports = {
+  extends: ['gitmoji'],
+  rules: {
+    'header-max-length': [0, 'always', 100],
+    'scope-case': [0, 'always', 'pascal-case'],
+  },
+}
