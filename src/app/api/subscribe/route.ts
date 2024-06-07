8c7f86501035f1ecf2b50d698f94af8e4de4f364
Vinicius Colares <dev@viniciuscolares.com>
2024-06-07 17:28:53 -0300

    fix(env): convertkit

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,38 +1,38 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request, res: Response) {
-  const { email } = await req.json();
+	const { email } = await req.json();
 
-  if (!email) {
-    return NextResponse.json({ error: "Email is required" }, { status: 400 });
-  }
+	if (!email) {
+		return NextResponse.json({ error: "Email is required" }, { status: 400 });
+	}
 
-  const API_KEY = process.env.NEXT_PUBLIC_CONVERTKIT_API_KEY;
-  const FORM_ID = process.env.NEXT_PUBLIC_CONVERTKIT_FORM_ID;
+	const API_KEY = process.env.CONVERTKIT_API_KEY;
+	const FORM_ID = process.env.CONVERTKIT_FORM_ID;
 
-  const data = {
-    email,
-    api_key: API_KEY,
-  };
+	const data = {
+		email,
+		api_key: API_KEY,
+	};
 
-  const response = await fetch(
-    `https://api.convertkit.com/v3/forms/${FORM_ID}/subscribe`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-      },
-      body: JSON.stringify(data),
-    }
-  );
+	const response = await fetch(
+		`https://api.convertkit.com/v3/forms/${FORM_ID}/subscribe`,
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json; charset=utf-8",
+			},
+			body: JSON.stringify(data),
+		}
+	);
 
-  if (response.ok) {
-    return NextResponse.json({ data: response.json() }, { status: 201 });
-  } else {
-    const errorData = await response.json();
-    return NextResponse.json(
-      { error: errorData.error || "Something went wrong" },
-      { status: 400 }
-    );
-  }
+	if (response.ok) {
+		return NextResponse.json({ data: response.json() }, { status: 201 });
+	} else {
+		const errorData = await response.json();
+		return NextResponse.json(
+			{ error: errorData.error || "Something went wrong" },
+			{ status: 400 }
+		);
+	}
 }
